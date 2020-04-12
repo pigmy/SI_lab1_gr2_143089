1f03f9f4ea6b3f6c4bcd72b42074c99cfe5ce08c
Stefan <nikolovski.stefan.2@students.finki.ukim.mk>
2020-04-12 11:04:55 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -1,12 +1,17 @@
+package test;
+
+import java.util.ArrayList;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
+	ArrayList<Integer> grades= new ArrayList<Integer>();
 
 	//TODO constructor
-
+	
 	//TODO seters & getters
-
+	
 	public double getAverage() {
 		//TODO
 	}
